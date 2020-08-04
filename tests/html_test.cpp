750183ab04139c949c6d75fa483d9549910a3187
Stephan Schedler <schedler@paderborn.com>
2020-08-04 15:17:50 +0200

    Add HtmlTable classes

--- tests/html_test.cpp
+++ tests/html_test.cpp
@@ -1,19 +1,6 @@
 #include "UserException.h"
 #include "Utils.h"
 #include "html/HtmlPage.h"
-#include "html/HtmlTitle.h"
-#include "html/HtmlText.h"
-#include "html/HtmlHeading.h"
-#include "html/HtmlLink.h"
-#include "html/HtmlParagraph.h"
-#include "html/HtmlDivision.h"
-#include "html/HtmlHeader.h"
-#include "html/HtmlMain.h"
-#include "html/HtmlBold.h"
-#include "html/HtmlFooter.h"
-#include "html/HtmlImage.h"
-#include "html/HtmlBreak.h"
-
 
 #include <fmt/format.h>
 
@@ -23,7 +10,6 @@
 #include <map>
 #include <ostream>
 #include <string>
-
 
 using namespace hokee;
 
@@ -37,38 +23,73 @@ int main(int /*unused*/, const char** /*unused*/)
         fs::path htmlPath = Utils::GetTempDir() / "index.html";
         std::ofstream htmlStream;
         htmlStream.open(htmlPath, std::ios::binary);
-        
+
         auto head = std::make_unique<HtmlHead>();
         head->AddElement(std::make_unique<HtmlTitle>("Title"));
         auto body = std::make_unique<HtmlBody>();
 
         auto header = std::make_unique<HtmlHeader>();
 
-        auto h2 = std::make_unique<HtmlHeading>(2,"Heading 2");
+        auto h2 = std::make_unique<HtmlHeading>(2, "Heading 2");
         header->AddElement(std::move(h2));
-        
+
         body->AddElement(std::move(header));
-        
+
         auto main = std::make_unique<HtmlMain>();
-        
-        auto p = std::make_unique<HtmlParagraph>();
-        p->AddElement("AAA BBB");
-        p->AddElement(std::make_unique<HtmlBreak>());
-        p->AddElement("CCC <escaped> DDD \"EEE\" GGG.");
-        main->AddElement(std::move(p));
-  
-        auto img = std::make_unique<HtmlImage>("https://www.fillmurray.com/200/100","fillmurray", 200, 100);
-        main->AddElement(std::move(img));
-      
+
+        auto div = std::make_unique<HtmlDivision>();
+
+        auto table = std::make_unique<HtmlTable>();
+
+        auto tableHeader = std::make_unique<HtmlTableRow>();
+
+        auto tableHead1 = std::make_unique<HtmlTableHeader>();
+        tableHead1->AddElement("Head 1");
+        tableHeader->AddElement(std::move(tableHead1));
+
+        auto tableHead2 = std::make_unique<HtmlTableHeader>();
+        tableHead2->AddElement("Head 2");
+        tableHeader->AddElement(std::move(tableHead2));
+
+        table->AddElement(std::move(tableHeader));
+
+        auto tableRow1 = std::make_unique<HtmlTableRow>();
+
+        auto tableCell1 = std::make_unique<HtmlTableCell>();
+
+        tableCell1->AddElement("AAA BBB");
+        tableCell1->AddElement(std::make_unique<HtmlBreak>());
+        tableCell1->AddElement("CCC <escaped>");
+        tableCell1->AddElement(std::make_unique<HtmlBreak>());
+        tableCell1->AddElement("DDD \"EEE\"");
+        tableCell1->AddElement(std::make_unique<HtmlBreak>());
+        tableCell1->AddElement("GGG.");
+
+        tableRow1->AddElement(std::move(tableCell1));
+
+        auto tableCell2 = std::make_unique<HtmlTableCell>();
+
+        tableCell2->AddElement(
+            std::make_unique<HtmlImage>("https://www.fillmurray.com/200/250", "fillmurray", 200, 250));
+
+        tableRow1->AddElement(std::move(tableCell2));
+
+        table->AddElement(std::move(tableRow1));
+
+        div->AddElement(std::move(table));
+
+        main->AddElement(std::move(div));
+
         body->AddElement(std::move(main));
 
         auto footer = std::make_unique<HtmlFooter>();
 
-        auto div = std::make_unique<HtmlDivision>();
-        div->AddElement(std::make_unique<HtmlBold>("BOLD: "));
-        div->AddElement(std::make_unique<HtmlLink>("https://www.fillmurray.com", "Link to fillmurray.com", "www.fillmurray.com"));
-        footer->AddElement(std::move(div));
-        
+        auto p = std::make_unique<HtmlParagraph>();
+        p->AddElement(std::make_unique<HtmlBold>("BOLD: "));
+        p->AddElement(std::make_unique<HtmlLink>("https://www.fillmurray.com", "Link to fillmurray.com",
+                                                 "www.fillmurray.com"));
+        footer->AddElement(std::move(p));
+
         body->AddElement(std::move(footer));
 
         HtmlPage htmlPage(std::move(head), std::move(body));
