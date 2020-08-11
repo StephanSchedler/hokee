6aae8acd6a449a7e5bf0bac63b221e9311d691d8
Stephan Schedler <schedler@paderborn.com>
2020-08-11 20:13:16 +0200

    do not escape HtmlText

--- src/html/HtmlText.cpp
+++ src/html/HtmlText.cpp
@@ -9,29 +9,6 @@ HtmlText::HtmlText(const std::string& text)
 
 void HtmlText::ToString(std::ostream& output) const
 {
-    for (size_t pos = 0; pos != _text.size(); ++pos)
-    {
-        switch (_text[pos])
-        {
-            case '&':
-                output << "&amp;";
-                break;
-            case '\"':
-                output << "&quot;";
-                break;
-            case '\'':
-                output << "&apos;";
-                break;
-            case '<':
-                output << "&lt;";
-                break;
-            case '>':
-                output << "&gt;";
-                break;
-            default:
-                output << _text[pos];
-                break;
-        }
-    }
+    output << _text;
 }
 } // namespace hokee
